9c8c0df54eccad50574a212f6f68cc686b40245a
root <root@localhost.localdomain>
2018-07-21 15:31:49 -0700

    refactor

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,85 +1,87 @@
 package com.tutorial.soa.core.crypto;
 
 public class VigenereCipher {
-	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	/**
 	 * @param string to encrypt
-	 * @param key
+	 * @param password
 	 * 
 	 * @return
 	 */
-	public static synchronized String encrypt(String string, String key) {
+	public static synchronized String encrypt(String string, String password) {
 		StringBuilder stringBuilder = new StringBuilder("");
-		
+
+		string = string.replaceAll(" ", "").toUpperCase();
+		password = password.replaceAll(" ", "").toUpperCase();
+
 		int j = 0;
 		for (int i = 0; i < string.length(); i++) {
-			if (j >= key.length()) {
+			int stringIndex = alphabet.indexOf(string.charAt(i));
+			int passwordIndex = alphabet.indexOf(password.charAt(j));
+			int offset = stringIndex + passwordIndex;
+
+			if (offset >= alphabet.length()) {
+				offset -= alphabet.length();
+			}
+
+			// Add space every 5th character
+			if ((i % 5) == 0) {
+				stringBuilder.append(" ");
+			}
+
+			stringBuilder.append(alphabet.charAt(offset));
+			j++;
+
+			if (j >= password.length()) {
 				j = 0;
 			}
-			
-			char currentChar = string.charAt(i);
-			
-			if (currentChar != ' ') {
-				int index = alphabet.indexOf(Character.toLowerCase(currentChar)) + alphabet.indexOf(key.toLowerCase().charAt(j));
-				
-				if (index >= alphabet.length()) {
-					index -= alphabet.length();
-				}
-				
-				if (!Character.isUpperCase(currentChar)) {
-					currentChar = alphabet.charAt(index);
-				}
-				else {
-					currentChar = Character.toUpperCase(alphabet.charAt(index));
-				}
-				
-				j++;
-			}
-			
-			stringBuilder.append(currentChar);
 		}
-		
+
+		// Remove 1st leading space
+		stringBuilder.delete(0, 1);
+
 		return stringBuilder.toString();
 	}
 	
 	/**
 	 * @param string to decrypt
-	 * @param key
+	 * @param password
 	 * 
 	 * @return
 	 */
-	public static synchronized String decrypt(String string, String key) {
+	public static synchronized String decrypt(String string, String password) {
 		StringBuilder stringBuilder = new StringBuilder("");
-		
+
+		string = string.replaceAll(" ", "").toUpperCase();
+		password = password.replaceAll(" ", "").toUpperCase();
+
 		int j = 0;
 		for (int i = 0; i < string.length(); i++) {
-			if (j >= key.length()) {
+			int stringIndex = alphabet.indexOf(string.charAt(i));
+			int passwordIndex = alphabet.indexOf(password.charAt(j));
+			int offset = stringIndex - passwordIndex;
+
+			if (offset < 0) {
+				offset += alphabet.length();
+			}
+
+			// Add space every 5th character
+			if ((i % 5) == 0) {
+				stringBuilder.append(" ");
+			}
+
+			stringBuilder.append(alphabet.charAt(offset));
+			j++;
+
+			if (j >= password.length()) {
 				j = 0;
 			}
-			
-			char currentChar = string.charAt(i);
-			
-			if (currentChar != ' ') {
-				int index = alphabet.indexOf(Character.toLowerCase(currentChar)) - alphabet.indexOf(key.toLowerCase().charAt(j));
-				
-				if (index < 0) {
-					index += alphabet.length();
-				}
-				
-				if (!Character.isUpperCase(currentChar)) {
-					currentChar = alphabet.charAt(index);
-				}
-				else {
-					currentChar = Character.toUpperCase(alphabet.charAt(index));
-				}
-				
-				j++;
-			}
-			
-			stringBuilder.append(currentChar);
 		}
-		
+
+		// Remove 1st leading space
+		stringBuilder.delete(0, 1);
+
 		return stringBuilder.toString();
 	}
 }
